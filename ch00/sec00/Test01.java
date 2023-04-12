2cdd4992ff63ed3497e70903f62a13cc7cfde897
catnparrot <idiotpi3rrot@gmail.com>
2023-04-12 16:12:44 +0900

    출력테스트

---
+++ ch00/sec00/Test01.java
@@ -0,0 +1,12 @@
+package ch00.sec00;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// 문자 출력
+		System.out.println("Hello Java");
+		System.out.println("자바: 나다");
+		
+	}
+
+}
